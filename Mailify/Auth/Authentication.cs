aae6eef443a7ce68119f935a6f025a99af336f6f
YoBoi <72236304+YoBoiiiiii@users.noreply.github.com>
2021-04-19 09:19:29 -0400

    Update Authentication.cs

--- Mailify/Auth/Authentication.cs
+++ Mailify/Auth/Authentication.cs
@@ -1,20 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using Leaf.xNet;
-using System.Management;
-using System.Reflection;
-using static Mailify.ConsoleUtilities;
-using System.Threading;
-using System.Windows;
-using System.Drawing;
-using AuthGG;
-using System.IO.Compression;
+
 
 namespace Mailify
 {
